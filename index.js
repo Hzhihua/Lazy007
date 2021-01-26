fcfd8fc5cd2aeccc461acda89984e6c364babf04
黄志华 <cnzhihua@gmail.com>
2021-01-26 16:53:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         网课
 // @namespace    Lazy007
-// @version      0.2
+// @version      0.1
 // @description  华南师范大学 - 一师一优课，一课一名师评比活动的教学提升技巧【自动跳转，自动关闭视频弹框。注意：评论不会自动回答】
 // @author       Lazy007
 // @match        http://www.scnunet.com/ncts/*
